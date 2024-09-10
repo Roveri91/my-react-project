849e3cd7582c1df7e2dc28d08c231f5012a7f9a4
Simone Roveri <spikeroveri91@gmail.com>
2024-09-10 20:51:06 +0900

    exercise: convert some html in JSX

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -1,9 +1,36 @@
 function Greeting() {
+  const animals = ['Lion', 'Cow', 'Snake', 'Lizard']
+  const animalsList = animals.map((animal) => <li key={animal}>animal</li>)
+
   return (
-    <div>
-      <h1>Hello my name is Simone</h1>
+    <div style={{ padding: "2rem"}}>
+      {/* first test */}
+      <h1>Test</h1>
+      <svg>
+        <circle cx="25" cy="75" r="20" stroke="green" strokeWidth="2" />
+      </svg>
+      <form>
+        <input type="text" />
+      </form>
+      {/* second test */}
+      <div>
+        <h1>Animals: </h1>
+        <ul>
+          {animals.map((animal) => {
+            return <li key={animal}>{animal}</li>
+          })}
+        </ul>
+      </div>
+      {/* second test PRO */}
+      <div>
+        <h1>Animals: </h1>
+        <ul>
+          {animalsList}
+        </ul>
+      </div>
+
     </div>
   )
 }
 
-export default Greeting
+export default Greeting;
